bed8d3a7e357f3a9dfac83b057c9391052859677
dodiko1d <kirill.anufrienko@gmail.com>
2020-07-13 06:47:17 +0300

    Tests atexit handler.

--- test_main.py
+++ test_main.py
@@ -1,12 +1,13 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
 
 from database import Base
 from main import app
 from products_group.router import get_db as products_group_get_db
 from product.router import get_db as product_get_db
+import os
+import atexit
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
@@ -36,3 +37,10 @@ client = TestClient(app)
 
 from products_group.tests import test_products_group_creation
 from product.tests import test_product_creation
+
+
+def exit_handler():
+    os.unlink('test.db')
+
+
+atexit.register(exit_handler)
